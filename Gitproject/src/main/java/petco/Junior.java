cb0ad641a8977f01dc052016a6f8d8a87f12eb62
ADMIN <ADMIN@QA-NINJA-1>
2021-09-07 21:44:45 -0400

    First commit

---
+++ Gitproject/src/main/java/petco/Junior.java
@@ -0,0 +1,13 @@
+package petco;
+
+public class Junior {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Peanut is happy");
+		
+		
+		
+	}
+
+}
